df8c67a7d7c674c01ca18ecb77ec5c169d838f94
Tal <anlv.tan@tendcloud.com>
2015-02-28 18:41:52 +0800

    update examle.

--- example/app.js
+++ example/app.js
@@ -5,5 +5,10 @@ var app = koa();
 
 app.use(autoroute('/api/', __dirname + '/api'));
 
+// Yep, you can use it just the same way as koa-route.
+app.use(autoroute.get('/test', function *() {
+    this.body = 'haha';
+}));
+
 console.log('Test server is now listening on port 20080');
 app.listen(20080);
